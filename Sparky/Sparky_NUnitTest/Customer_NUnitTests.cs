fd61de38bfdb1d93439fc35e5a682a22366b7611
kentzurich <kentzurichanthony@gmail.com>
2023-10-23 15:17:16 +0800

    String Assert Helper Methods

--- Sparky/Sparky_NUnitTest/Customer_NUnitTests.cs
+++ Sparky/Sparky_NUnitTest/Customer_NUnitTests.cs
@@ -15,7 +15,14 @@ namespace Sparky
             string fullName = customer.GreetAndCombineNames("Kent", "Zurich");
 
             //Assert
+            Assert.AreEqual(fullName, "Hello, Kent Zurich");
+
             Assert.That(fullName, Is.EqualTo("Hello, Kent Zurich"));
+            Assert.That(fullName, Does.Contain("Kent Zurich"));
+            Assert.That(fullName, Does.Contain("kent zurich").IgnoreCase);
+            Assert.That(fullName, Does.StartWith("Hello"));
+            Assert.That(fullName, Does.EndWith("Zurich"));
+            Assert.That(fullName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
         }
     }
 }
