79f60644ea5ecfebd9cbc9acb7ffead8d8cfefa4
kentzurich <kentzurichanthony@gmail.com>
2023-10-23 13:39:17 +0800

    First NUnit Test

---
+++ Sparky/Sparky_NUnitTest/Calculator_NUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class Calculator_NUnitTests
+    {
+        [Test]
+        public void AddNumber_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange - Initialization
+            Calculator calc = new();
+
+            //Act - Invoke method to test
+            int result = calc.AddNumber(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+    }
+}
