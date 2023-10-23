f31f02dee1bfa0c6c17957aedd75759ff631f0f2
kentzurich <kentzurichanthony@gmail.com>
2023-10-23 14:32:58 +0800

    Asseting Doubles

--- Sparky/Sparky/Calculator.cs
+++ Sparky/Sparky/Calculator.cs
@@ -7,10 +7,14 @@
             return a + b;
         }
 
+        public double AddNumbersDouble(double a, double b)
+        {
+            return a + b;
+        }
+
         public bool IsOddNumber(int a)
         {
             return (a % 2 != 0);
         }
-
     }
 }
--- Sparky/Sparky_NUnitTest/Calculator_NUnitTests.cs
+++ Sparky/Sparky_NUnitTest/Calculator_NUnitTests.cs
@@ -54,5 +54,21 @@ namespace Sparky
 
             return calc.IsOddNumber(a);
         }
+
+        [Test]
+        [TestCase(5.4, 10.5)] //15.9
+        [TestCase(5.43, 10.53)] //15.96
+        [TestCase(5.49, 10.59)] //16.08
+        public void AddNumbersDouble_InputTwoDouble_GetCorrectAddition(double a, double b)
+        {
+            //Arrange - Initialization
+            Calculator calc = new();
+
+            //Act - Invoke method to test
+            double result = calc.AddNumbersDouble(a, b);
+
+            //Assert
+            Assert.AreEqual(15.9, result, .2);
+        }
     }
 }
