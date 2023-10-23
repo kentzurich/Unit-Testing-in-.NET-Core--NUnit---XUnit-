b71039cf52b60ee0b5cf359261474616da5dfd7b
kentzurich <kentzurichanthony@gmail.com>
2023-10-23 10:32:02 +0800

    First Unit Test

---
+++ Sparky/Sparky_MSTest/Calculator_MSTests.cs
@@ -0,0 +1,21 @@
+﻿using Sparky;
+
+namespace Sparky_MSTest
+{
+    [TestClass]
+    public class Calculator_MSTests
+    {
+        [TestMethod]
+        public void AddNumber_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange - Initialization
+            Calculator calc = new();
+
+            //Act - Invoke method to test
+            int result = calc.AddNumber(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+    }
+}
