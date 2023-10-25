bc401137fa661ba00697a206b0a20ea886f6d196
kentzurich <kentzurichanthony@gmail.com>
2023-10-25 14:40:17 +0800

    Setup Inheritance

--- Sparky/Sparky/Customer.cs
+++ Sparky/Sparky/Customer.cs
@@ -3,6 +3,7 @@
     public class Customer
     {
         public int Discount = 15;
+        public int OrderTotal { get; set; }
         public string GreetMessage { get; set; }
 
         public string GreetAndCombineNames(string firstName, string lastName)
@@ -13,5 +14,16 @@
             Discount = 20;
             return GreetMessage;
         }
+
+        public CustomerType GetCustomerDetails()
+        {
+            if (OrderTotal < 100) return new BasicCustomer();
+
+            return new PlatinumCustomer();
+        }
     }
+
+    public class CustomerType { }
+    public class BasicCustomer : CustomerType { }
+    public class PlatinumCustomer: CustomerType { }
 }
