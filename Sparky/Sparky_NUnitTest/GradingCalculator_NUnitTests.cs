8e7ea7dccb6634893ae6299db7d6f7ec7087c7ce
kentzurich <kentzurichanthony@gmail.com>
2023-10-25 15:13:00 +0800

    Assignment 2 - Grading Calculator Part 1

---
+++ Sparky/Sparky_NUnitTest/GradingCalculator_NUnitTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class GradingCalculator_NUnitTests
+    {
+        private GradingCalculator _gradingCalc;
+        [SetUp]
+        public void SetUp()
+        {
+            _gradingCalc = new GradingCalculator();
+        }
+
+        [Test]
+        public void GradeChecker_InputScore95AndAttendance90_ReturnGradeA()
+        {
+            _gradingCalc.Score = 95;
+            _gradingCalc.AttendancePercentage = 90;
+
+            var result = _gradingCalc.GetGrade();
+
+            Assert.That(result, Is.EqualTo("A"));
+        }
+
+        [Test]
+        public void GradeChecker_InputScore85AndAttendance90_ReturnGradeB()
+        {
+            _gradingCalc.Score = 85;
+            _gradingCalc.AttendancePercentage = 90;
+
+            var result = _gradingCalc.GetGrade();
+
+            Assert.That(result, Is.EqualTo("B"));
+        }
+
+        [Test]
+        public void GradeChecker_InputScore95AndAttendance65_ReturnGradeB_2()
+        {
+            _gradingCalc.Score = 95;
+            _gradingCalc.AttendancePercentage = 65;
+
+            var result = _gradingCalc.GetGrade();
+
+            Assert.That(result, Is.EqualTo("B"));
+        }
+
+        [Test]
+        public void GradeChecker_InputScore65AndAttendance90_ReturnGradeC()
+        {
+            _gradingCalc.Score = 65;
+            _gradingCalc.AttendancePercentage = 90;
+
+            var result = _gradingCalc.GetGrade();
+
+            Assert.That(result, Is.EqualTo("C"));
+        }
+
+        [Test]
+        [TestCase(95, 55, ExpectedResult = "F")]
+        [TestCase(75, 55, ExpectedResult = "F")]
+        [TestCase(50, 90, ExpectedResult = "F")]
+        public string GradeChecker_InputMultipleScoreAndAttendance_ReturnGradeF(int score, int attendancePercentage)
+        {
+            _gradingCalc.Score = score;
+            _gradingCalc.AttendancePercentage = attendancePercentage;
+
+            return _gradingCalc.GetGrade();
+        }
+    }
+}
