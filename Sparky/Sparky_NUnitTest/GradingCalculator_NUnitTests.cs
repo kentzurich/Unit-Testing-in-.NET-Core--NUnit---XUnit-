0c0958417c2f82c92fc813ee003c6f3e24e8712f
kentzurich <kentzurichanthony@gmail.com>
2023-10-25 15:21:00 +0800

    Assignment 2 - Grading Calculator Part 2

--- Sparky/Sparky_NUnitTest/GradingCalculator_NUnitTests.cs
+++ Sparky/Sparky_NUnitTest/GradingCalculator_NUnitTests.cs
@@ -57,10 +57,28 @@ namespace Sparky
         }
 
         [Test]
+        [TestCase(95, 55)]
+        [TestCase(75, 55)]
+        [TestCase(50, 90)]
+        public void GradeChecker_InputMultipleScoreAndAttendance_ReturnGradeF(int score, int attendancePercentage)
+        {
+            _gradingCalc.Score = score;
+            _gradingCalc.AttendancePercentage = attendancePercentage;
+
+            var result = _gradingCalc.GetGrade();
+
+            Assert.That(result, Is.EqualTo("F"));
+        }
+
+        [Test]
+        [TestCase(95, 90, ExpectedResult = "A")]
+        [TestCase(85, 90, ExpectedResult = "B")]
+        [TestCase(65, 90, ExpectedResult = "C")]
+        [TestCase(95, 65, ExpectedResult = "B")]
         [TestCase(95, 55, ExpectedResult = "F")]
         [TestCase(75, 55, ExpectedResult = "F")]
         [TestCase(50, 90, ExpectedResult = "F")]
-        public string GradeChecker_InputMultipleScoreAndAttendance_ReturnGradeF(int score, int attendancePercentage)
+        public string GradeChecker_InputMultipleScoreAndAttendance_ReturnGrade(int score, int attendancePercentage)
         {
             _gradingCalc.Score = score;
             _gradingCalc.AttendancePercentage = attendancePercentage;
