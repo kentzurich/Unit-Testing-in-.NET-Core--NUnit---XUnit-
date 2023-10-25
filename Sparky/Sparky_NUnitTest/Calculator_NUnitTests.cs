159537b3b4e8b202ac71afd773bcd68609576680
kentzurich <kentzurichanthony@gmail.com>
2023-10-25 11:36:03 +0800

    Collections Helper Method

--- Sparky/Sparky_NUnitTest/Calculator_NUnitTests.cs
+++ Sparky/Sparky_NUnitTest/Calculator_NUnitTests.cs
@@ -75,12 +75,23 @@ namespace Sparky
         [Test]
         public void OddRanger_InputMinAndMaxRange_ReturnsValidOddNumberRange()
         {
+            //Arrange
             Calculator calc = new();
             List<int> expectedOddRange = new() { 5, 7, 9 }; //5-10
 
+            //Act
             List<int> result = calc.GetOddRange(5, 10);
 
+            //Assert
             Assert.That(result, Is.EquivalentTo(expectedOddRange));
+            //Assert.AreEqual(expectedOddRange, result);
+            //Assert.Contains(7, result);
+            Assert.That(result, Does.Contain(7));
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result.Count, Is.EqualTo(3));
+            Assert.That(result, Has.No.Member(6));
+            Assert.That(result, Is.Ordered.Ascending);
+            Assert.That(result, Is.Unique);
         }
     }
 }
