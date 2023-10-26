405c52c89de2bba5cd1705c3193d9454dc1004e6
kentzurich <kentzurichanthony@gmail.com>
2023-10-26 10:21:29 +0800

    Integration Test Demo

---
+++ Sparky/Sparky_NUnitTest/BankAccount_NUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class BankAccount_NUnitTests
+    {
+        private BankAccount bankAccount;
+        [SetUp] 
+        public void SetUp() 
+        {
+            bankAccount = new(new LogBook());
+        }
+
+        [Test]
+        public void BankDeposit_Add100_ReturnTrue()
+        {
+            var result = bankAccount.Deposit(100);
+            var getBalance = bankAccount.GetBalance();
+
+            Assert.That(result, Is.True);
+            Assert.That(getBalance, Is.EqualTo(100));
+        }
+    }
+}
